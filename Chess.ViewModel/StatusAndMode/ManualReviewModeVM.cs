603fd4fb977d9ae86f799902bb5ec39fcfc7d144
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2025-08-27 20:14:39 +0530

    Added more info

--- Chess.ViewModel/StatusAndMode/ManualReviewModeVM.cs
+++ Chess.ViewModel/StatusAndMode/ManualReviewModeVM.cs
@@ -5,18 +5,85 @@ namespace Chess.ViewModel.StatusAndMode
 {
     public class ManualReviewModeVM : ObservableObject
     {
+        private readonly GenericCommand undoOriginalCommand;
+
+        private readonly GenericCommand redoOriginalCommand;
+
         private readonly GenericCommand undoCommand;
 
         private readonly GenericCommand redoCommand;
+
+        private readonly GenericCommand getToStartCommand;
+
+        private readonly GenericCommand getToLastCommand;
+
         public ManualReviewModeVM(GenericCommand undoCommand, GenericCommand redoCommand)
         {
-            this.undoCommand = undoCommand;
+            undoOriginalCommand = undoCommand;
 
-            this.redoCommand = redoCommand;
+            redoOriginalCommand = redoCommand;
+
+            this.undoCommand = new GenericCommand(() => undoCommand.CanExecute(null), Undo);
+
+            this.redoCommand = new GenericCommand(() => redoCommand.CanExecute(null), Redo);
+
+            this.getToStartCommand = new GenericCommand(() => undoCommand.CanExecute(null), GetToStart);
+
+            this.getToLastCommand = new GenericCommand(() => redoCommand.CanExecute(null), GetToLast);
         }
 
         public GenericCommand UndoCommand => this.undoCommand;
 
         public GenericCommand RedoCommand => this.redoCommand;
+
+        public GenericCommand GetToStartCommand => this.getToStartCommand;
+
+        public GenericCommand GetToLastCommand => this.getToLastCommand;
+
+        private void Undo()
+        {
+            this.undoOriginalCommand.Execute(null);
+            RaiseCanExecuteChanged();
+        }
+
+        private void Redo()
+        {
+            this.redoOriginalCommand.Execute(null);
+            RaiseCanExecuteChanged();
+        }
+
+        public void GetToStart()
+        {
+            while (this.undoCommand.CanExecute(null))
+            {
+                this.undoCommand.Execute(null);
+            }
+            RaiseCanExecuteChanged();
+
+            // First, oldest, farthest 
+            ChessAppSettings.Default.ReviewFromLast = false;
+            ChessAppSettings.Default.Save();
+        }
+
+        public void GetToLast()
+        {
+            while (this.redoCommand.CanExecute(null))
+            {
+                this.redoCommand.Execute(null);
+            }
+            RaiseCanExecuteChanged();
+
+            // Last, most recent, closest 
+            ChessAppSettings.Default.ReviewFromLast = true;
+            ChessAppSettings.Default.Save();
+        }
+
+        private void RaiseCanExecuteChanged()
+        {
+            this.undoCommand.FireCanExecuteChanged();
+            this.redoCommand.FireCanExecuteChanged();
+            this.getToStartCommand.FireCanExecuteChanged();
+            this.getToLastCommand.FireCanExecuteChanged();
+        }
     }
 }
