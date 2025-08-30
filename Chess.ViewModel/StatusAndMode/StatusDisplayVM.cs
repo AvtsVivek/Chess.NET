1277a8b4d153dfa4a017a26c583ee8c6b626fe37
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2025-08-30 15:48:18 +0530

    Board Inversion button moved out

--- Chess.ViewModel/StatusAndMode/StatusDisplayVM.cs
+++ Chess.ViewModel/StatusAndMode/StatusDisplayVM.cs
@@ -9,31 +9,10 @@ namespace Chess.ViewModel.StatusAndMode
         [ObservableProperty]
         private string statusText;
 
-        private GenericCommand toggleBoardInversionCommand;
-
-        /// <summary>
-        /// Represents the undo command, which reverts to a previous game state.
-        /// </summary>
-        private readonly GenericCommand invertBoardCommand;
-        public StatusDisplayVM(Status status, GenericCommand toggleBoardInversionCommand)
+        public StatusDisplayVM(Status status)
         {
             UpdateStatus(status);
-
-            this.invertBoardCommand = new GenericCommand(() => true, InvertBoard);
-
-            this.toggleBoardInversionCommand = toggleBoardInversionCommand;
         }
-
-        private void InvertBoard()
-        {
-            toggleBoardInversionCommand.Execute(null);
-        }
-
-        /// <summary>
-        /// Gets the command that reverts the last action of the presented chess game.
-        /// </summary>
-        /// <value>The command that reverts the last action of the presented chess game.</value>
-        public GenericCommand InvertBoardCommand => this.invertBoardCommand;
 
         public void UpdateStatus(Status status)
         {
